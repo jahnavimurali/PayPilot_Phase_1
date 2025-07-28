5e256ba2680d28953f8b756d51c5a6b11659ce78
Saloniii7 <118054187+Saloniii7@users.noreply.github.com>
2025-07-28 16:07:14 +0530

    sort bills by due date

--- src/com/paypilot/util/BillSorter.java
+++ src/com/paypilot/util/BillSorter.java
@@ -1,8 +1,11 @@
+
+package com.paypilot.util;
 import java.util.*;
+import com.paypilot.model.Bill;
 
 public class BillSorter {
 
-    public static List<Bill> getSortedBillsCopy(List<Bill> bills) {
+    public static List<Bill> SortedBillsByDueDate(List<Bill> bills) {
         List<Bill> sortedBills = new ArrayList<>(bills);  
         sortedBills.sort(Comparator.comparing(Bill::getDueDate)); 
         return sortedBills;
