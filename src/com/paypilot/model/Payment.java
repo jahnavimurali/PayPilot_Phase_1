503eb3df4c9b413a5b3e1b6a439c9a5fa57e77ba
RithikGarg22 <161447652+RithikGarg22@users.noreply.github.com>
2025-07-28 16:02:37 +0530

    Update Payment.java

--- src/com/paypilot/model/Payment.java
+++ src/com/paypilot/model/Payment.java
@@ -1,23 +1,40 @@
 package com.paypilot.model;
 
 import java.io.Serializable;
+import java.time.LocalDate;
 import java.util.Objects;
 
 public class Payment implements Serializable {
-    private String billId;
-    private double amountPaid;
 
-    public Payment(String billId, double amountPaid) {
+    private int paymentId;
+    private int billId;
+    private double amountPaid;
+    private LocalDate paymentDate;
+    private String mode;
+
+    // Constructor
+    public Payment(int paymentId, int billId, double amountPaid, LocalDate paymentDate, String mode) {
+        this.paymentId = paymentId;
         this.billId = billId;
         this.amountPaid = amountPaid;
+        this.paymentDate = paymentDate;
+        this.mode = mode;
     }
 
-    // Getters & Setters
-    public String getBillId() {
+    // Getters and Setters
+    public int getPaymentId() {
+        return paymentId;
+    }
+
+    public void setPaymentId(int paymentId) {
+        this.paymentId = paymentId;
+    }
+
+    public int getBillId() {
         return billId;
     }
 
-    public void setBillId(String billId) {
+    public void setBillId(int billId) {
         this.billId = billId;
     }
 
@@ -29,25 +46,50 @@ public class Payment implements Serializable {
         this.amountPaid = amountPaid;
     }
 
+    public LocalDate getPaymentDate() {
+        return paymentDate;
+    }
+
+    public void setPaymentDate(LocalDate paymentDate) {
+        this.paymentDate = paymentDate;
+    }
+
+    public String getMode() {
+        return mode;
+    }
+
+    public void setMode(String mode) {
+        this.mode = mode;
+    }
+
+    // equals method for JUnit and object comparison
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof Payment)) return false;
         Payment payment = (Payment) o;
-        return Double.compare(payment.amountPaid, amountPaid) == 0 &&
-                Objects.equals(billId, payment.billId);
+        return paymentId == payment.paymentId &&
+                billId == payment.billId &&
+                Double.compare(payment.amountPaid, amountPaid) == 0 &&
+                Objects.equals(paymentDate, payment.paymentDate) &&
+                Objects.equals(mode, payment.mode);
     }
 
+    // hashCode for proper collection support
     @Override
     public int hashCode() {
-        return Objects.hash(billId, amountPaid);
+        return Objects.hash(paymentId, billId, amountPaid, paymentDate, mode);
     }
 
+    // toString for readable output
     @Override
     public String toString() {
         return "Payment{" +
-                "billId='" + billId + '\'' +
+                "paymentId=" + paymentId +
+                ", billId=" + billId +
                 ", amountPaid=" + amountPaid +
+                ", paymentDate=" + paymentDate +
+                ", mode='" + mode + '\'' +
                 '}';
     }
 }
