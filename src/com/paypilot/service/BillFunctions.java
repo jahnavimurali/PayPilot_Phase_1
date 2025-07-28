b166086798d9ff97d6ab14c9ad685c53e7e30e26
Excalibur02 <dhruv.sharma.ug21@nsut.ac.in>
2025-07-28 15:42:14 +0530

    changed getOverDueBills

--- src/com/paypilot/service/BillFunctions.java
+++ src/com/paypilot/service/BillFunctions.java
@@ -115,7 +115,8 @@ public class BillFunctions {
     }
 
     // This method returns a list of all overdue bills
-    public static List<Bill> getOverDueBills(List<Bill> bills){
+    public static List<Bill> getOverDueBills(){
+        List<Bill> bills = new BillFunctions().findAllBills(); // Assuming this retrieves all bills
         List<Bill> overdueBills = new ArrayList<>();
         LocalDate currentDate = LocalDate.now();
 
